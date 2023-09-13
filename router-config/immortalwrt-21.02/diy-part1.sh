320e4f5b50401d937f0630448e82bfae75bc3af9
thangcualo <122159126+thangcualo@users.noreply.github.com>
2023-09-13 08:35:19 +0700

    Update diy-part1.sh

--- router-config/immortalwrt-21.02/diy-part1.sh
+++ router-config/immortalwrt-21.02/diy-part1.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-#========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
-# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: openwrt-21.02
-#========================================================================================================================
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# other
-# rm -rf package/emortal/{autosamba,ipv6-helper}
-
+# Add packages from kenzok8/small-package
+git clone --depth=1 https://github.com/kenzok8/small-package package/small-package
